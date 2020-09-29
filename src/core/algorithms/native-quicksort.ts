ca0e6abfea648e1665018b25217be0c9d935076a
rneto <rrnoak@gmail.com>
2020-09-29 07:18:30 +0200

    docs: Add explanation about the native quicksort

--- src/core/algorithms/native-quicksort.ts
+++ src/core/algorithms/native-quicksort.ts
@@ -4,6 +4,7 @@ import { SorterAlgorithmInterface } from '.';
 
 /**
  * Native quicksort algorithm implementation.
+ * Chrome v8 uses QuickSort for Array.sort implementation.
  */
 export class NativeQuicksort implements SorterAlgorithmInterface {
   sort(numbers: number[]): void {
